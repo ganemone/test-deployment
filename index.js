0ac09cb53ebb54d3e86c215522cee3371e498959
Giancarlo Anemone <ganemone@uber.com>
2019-05-15 12:42:47 -0400

    Configure now.json

---
+++ index.js
@@ -0,0 +1,10 @@
+// const getHandler = require('fusion-cli/serverless');
+
+// module.exports = getHandler({
+//   // dir: '.', // optional
+//   // env: 'production', // optional
+// });
+
+module.exports = function(req, res) {
+  res.end('OK');
+}
