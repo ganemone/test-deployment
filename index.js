9e62caad5023b70471fdf250350311540dfebe9a
Giancarlo Anemone <ganemone@uber.com>
2019-05-15 15:00:41 -0400

    Update logs

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 const fs = require('fs');
 
 module.exports = function(req, res) {
-  console.log('CWD', process.cwd());
-  console.log('__DIRNAME', __dirname);
+  console.log('CWD', fs.readdirSync(process.cwd()));
+  console.log('__DIRNAME', fs.readdirSync(__dirname));
   // const dirs = fs.readdirSync('.fusion/dist/production/server');
   // res.end('DIRS' + JSON.stringify(dirs));
   res.end('OK');
